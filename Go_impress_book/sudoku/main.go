539a5698f9da2d68a60aa6742d7b3b4c97c68ccd
fish0504 <k_hiro@kzd.biglobe.ne.jp>
2021-07-14 15:35:57 +0900

    ユーザの入力と縦方向のチェックを追加テスト済

--- Go_impress_book/sudoku/main.go
+++ Go_impress_book/sudoku/main.go
@@ -8,23 +8,14 @@ type Board struct{
 	nums [][] int
 	size int
 }
-func(b *Board) getExamBorad(){
-	b = &Board{
-		nums: [][]int{
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 1, 0, 3, 0, 0, 0, 0},
-				{0, 0, 0, 2, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-		},
-		size: 9,
+func  getCkeckLen(len int)([] bool){
+	alreadyExist:= make([]bool,len)
+		for p:=0;p<len;p++{
+		alreadyExist[p]=false
 	}
-	
+	return alreadyExist
 }
+
 func outBar(len int)(){
 	for i:=0;i<len+2;i++{
 		fmt.Printf("--")
@@ -58,33 +49,75 @@ func (b *Board)	get(x,y int)int{
 	return b.nums[y][x]
 }
 func (b *Board)	put(x,y,value int)bool{
-	x--
-	y--
+	
 	//すでに埋まっていたらだめ
 	if b.get(x,y)!=0 {
 		return false;
 	}else {
+		x--
+		y--
 		b.nums[y][x]=value
 		return true
 	}
+}
+func (b* Board)input(){
+	var x,y,value int
+	//座標を入力
+	fmt.Printf("Input (x,y):")
+	fmt.Scan(&x)
+	fmt.Scan(&y)
+
+	fmt.Printf("Input value:")
+	fmt.Scan(&value)
+	b.put(x,y,value)
+	b.show()
+}
+//縦方向について数字の重複がないか調べる
+// 重複があればfalse,重複がなければtrue
+func (b* Board)checkVer()bool{
+
+	for k:=1;k<=b.size;k++{
+		//1~9の数字が見つかっているか判定する配列
+		//3が見つかっている時
+		//exist[2]=true
+		exist:=getCkeckLen(b.size)
+		for i:=1;i<=b.size;i++{
+			//まだ数字が入っていない時
+			if b.get(k,i)==0{
+				continue
+			}else if (!exist[b.get(k,i)-1]){
+				exist[b.get(k,i)-1]=true
+			}else{
+				return false
+			}
+		}
+	}
+	return true
 
 }
 
 func main(){
 	b := &Board{
 		nums: [][]int{
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 1, 0, 3, 0, 0, 0, 0},
+			{0, 0, 0, 2, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		},
 		size: 9,
 	}
 	b.show()
-
+	for ; ;{
+		b.input()
+		if !b.checkVer(){
+			fmt.Println("failed!")
+			break
+		}
+	}
+	
 }
--- Go_impress_book/sudoku/main_test.go
+++ Go_impress_book/sudoku/main_test.go
@@ -1,7 +1,10 @@
 package main
 
-import "testing"
-
+import (
+	"fmt"
+	"testing"
+	//"golang.org/x/tools/go/expect"
+)
 
 //要素取得のget関数のtest
 func Test01(t *testing.T) {
@@ -80,7 +83,78 @@ func Test02(t *testing.T) {
 	} else if result3 != expected3 {
 		t.Errorf("Error!\n result3 != expected3")
 	} else if result4 !=expected4{	
-		t.Errorf("Error!\n result3 != expected3")
-	}
+		fmt.Printf("result4=%t\n",result4)
+		fmt.Printf("expected4=%t\n",expected4)
 
+		t.Errorf("Error!\n result4 != expected4 ")
+	}
 }
+//標準入力から座標と値を受け取って反映する
+func Test03(t *testing.T){
+	b := &Board{
+		nums: [][]int{
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+		size: 9,
+	}
+	// Input (x,y):2 2
+	// Input value:6
+	expected:=6
+	b.put(2,2,expected)
+	result:=b.get(2,2)
+	if expected!=result{
+		t.Errorf("Error!\n result != expected")
+	}
+}
+
+//
+func Test04(t *testing.T){
+	b := &Board{
+		nums: [][]int{
+			{1, 0, 3, 4, 5, 6, 7, 8, 9},
+			{0, 1, 0, 0, 0, 0, 0, 0, 0},
+			{3, 3, 0, 0, 0, 0, 0, 0, 0},
+			{4, 4, 0, 0, 0, 0, 0, 0, 0},
+			{5, 5, 0, 0, 0, 0, 0, 0, 0},
+			{6, 6, 0, 0, 0, 0, 0, 0, 0},
+			{7, 7, 0, 0, 0, 0, 0, 0, 0},
+			{8, 8, 0, 0, 0, 0, 0, 0, 0},
+			{9, 9, 0, 0, 0, 0, 0, 0, 0},
+		},
+		size: 9,
+	}
+	b2 := &Board{
+		nums: [][]int{
+			{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			{2, 1, 3, 4, 5, 6, 7, 8, 9},
+			{3, 3, 0, 0, 0, 0, 0, 0, 0},
+			{4, 4, 0, 0, 0, 0, 0, 0, 0},
+			{5, 5, 0, 0, 0, 0, 0, 0, 0},
+			{6, 6, 0, 0, 0, 0, 0, 0, 0},
+			{7, 7, 0, 0, 0, 0, 0, 0, 0},
+			{8, 8, 0, 0, 0, 0, 0, 0, 0},
+			{8, 9, 0, 0, 0, 0, 0, 0, 0},
+		},
+		size: 9,
+	}
+	expected1:=true
+	result1:=b.checkVer()
+	
+	expected2:=false
+	result2:=b2.checkVer()
+	if expected1!=result1{
+		t.Errorf("Error!\n result(%t) != expected(%t)",result1,expected1)
+	}else if expected2!=result2{
+		t.Errorf("Error!\n result2(%t) != expected2(%t)",result2,expected2)
+	}
+}
+
+
